f9c8563e20da3cb7c147aabd4b79f4c3022f0105
Mattia Piccinetti <mattia.piccinetti@satispay.com>
2019-12-21 22:01:11 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo
+echo "######################################################"
+echo "#                                                    #"
+echo "# Hi! Thank you for downloading the git-flow-aliases #" 
+echo "#                                                    #"
+echo "# GitHub:  https://github.com/mattiapiccinetti       #"
+echo "# Twitter: https://twitter.com/peachofpersia         #"
+echo "#                                                    #"
+echo "######################################################"
+echo
+
+FILENAME=".git-flow-functions"
+
+echo "# Downloading $FILENAME into your \$HOME directory"
+curl -sLo $HOME/$FILENAME \
+    https://raw.githubusercontent.com/mattiapiccinetti/git-flow-aliases/master/$FILENAME
+
+echo "# Adding git aliases to your global .gitconfig file"
+for alias in "feature" "release" "hotfix" "support" "close"
+do
+    git config --global alias.$alias "!bash -c 'source \$HOME/$FILENAME && $alias \$0'"
+done
+echo "# Done"
+echo
